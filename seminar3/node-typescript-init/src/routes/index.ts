050e2fb18368a49af98e992609a81e8d8882c999
2018033018 <rkdalswo1021@hanyang.ac.kr>
2022-04-28 14:02:37 +0900

    [Remove] .git 파일  제거

---
+++ seminar3/node-typescript-init/src/routes/index.ts
@@ -0,0 +1,10 @@
+//router index file
+import { Router } from 'express';
+import UserRouter from "./UserRouter";
+
+
+const router: Router = Router();
+
+router.use('/user', UserRouter);
+
+export default router;
